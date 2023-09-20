80be7e01f7fce84fbfc3b05101f0af4dd8f4cd79
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2023-09-20 08:15:39 +0200

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="demultiplexer",
-    version="1.1.2",
+    version="1.2.0",
     author="Dominik Buchner",
     author_email="dominik.buchner524@googlemail.com",
     description="python script to demultiplex illumina reads tagged with the leeselab tagging scheme",
@@ -13,22 +13,23 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/DominikBuchner/demultiplexer",
     packages=setuptools.find_packages(),
-    license = 'MIT',
-    install_requires = ['PySimpleGUI >= 4.19.0',
-                        'openpyxl >= 3.0.3',
-                        'psutil >= 5.7.3',
-                        'biopython >= 1.78',
-                        'joblib >= 0.16.0',
-                        ],
-    include_package_data = True,
+    license="MIT",
+    install_requires=[
+        "PySimpleGUI >= 4.19.0",
+        "openpyxl >= 3.0.3",
+        "psutil >= 5.7.3",
+        "biopython >= 1.78",
+        "joblib >= 0.16.0",
+    ],
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    entry_points = {
-        "console_scripts" : [
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
             "demultiplexer = demultiplexer.__main__:main",
         ]
     },
